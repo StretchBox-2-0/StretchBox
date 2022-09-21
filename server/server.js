5784b8601fecfcdb38ba4bbaa83d88b853b2093f
radizorit <jlimanalysis@gmail.com>
2022-09-21 12:42:30 -0700

    saving

--- server/server.js
+++ server/server.js
@@ -1,13 +1,10 @@
 const express = require('express');
-<<<<<<< HEAD
-const request = require('request')
-const database = require('./model');
-=======
+
 const request = require('request');
-const database = require ('./model');
+const database = require('./model');
 const dotenv = require('dotenv');
 const cookieParser = require('cookie-parser');
->>>>>>> dev
+
 const app = express();
 const port = 3000;
 dotenv.config();
@@ -17,21 +14,14 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 
-<<<<<<< HEAD
-app.post('/api', apiController.getExercises, (req, res) => {
-  // console.log('server res.locals.stretches', res.locals.stretches);
-  res.status(200).json(res.locals.stretches);
-});
-=======
 const workOutRoute = require('./routes/workOut');
 const userRoute = require('./routes/user');
 // const userRoute = require('./routes/user')
 
 // app.use('/workout', workOutRoute);
-app.use('/user', userRoute); 
+app.use('/user', userRoute);
 
 
->>>>>>> dev
 
 app.get('/', (req, res) => {
   res.sendStatus(200);
