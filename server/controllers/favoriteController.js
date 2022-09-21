ed01e76786317baf8ef72f830223796296bf441e
radizorit <jlimanalysis@gmail.com>
2022-09-21 13:24:05 -0700

    good stopping point

--- server/controllers/favoriteController.js
+++ server/controllers/favoriteController.js
@@ -4,23 +4,21 @@ const favoriteController = {};
 
 // get user
 favoriteController.getFavorite = async (req, res, next) => {
+  console.log(res.locals.user.userId, 'res.locals.user.userId')
   try {
     console.log('IN FAV')
-    //work with front end to get proper names
-    // const { stretchid, name } = req.body;
-    const userID = res.locals.user.userId;
-    //sql goes here
-    const query = `SELECT * from favorite where userid = ${userID};`
+    console.log(res.locals.user.userId, 'res.locals.user.userId')
+    const query = `SELECT * from favorite where userid = ${res.locals.user.userId};`
     const dbResults = await database.query(query);
-    console.log(dbResults, 'dbResults')
+    console.log(dbResults.rows, 'dbResults') //dbResults.rows
 
     // store userID to locals to sendback to front-end
     // res.locals.userID = insertedId;
     return next();
   } catch (err) {
     return next({
-      log: "error creating favorite exercises",
-      message: { err: `error creating favortie exercises ERROR: ${err}` },
+      log: "error getting favorite exercises",
+      message: { err: `error getting favortie exercises ERROR: ${err}` },
     });
   }
 };
@@ -41,8 +39,8 @@ favoriteController.deleteFavorite = async (req, res, next) => {
     return next();
   } catch (err) {
     return next({
-      log: "error creating favorite exercises",
-      message: { err: `error creating favortie exercises ERROR: ${err}` },
+      log: "error deleting favorite exercises",
+      message: { err: `error deleting favortie exercises ERROR: ${err}` },
     });
   }
 };
--- server/routes/favRoute.js
+++ server/routes/favRoute.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const favoriteControlller = require('../controllers/favoriteController.js');
+const favoriteController = require('../controllers/favoriteController.js');
 const jwtController = require('../controllers/jwtController.js')
 
 // create a fav 
-router.post('/',jwtController.verify, favoriteControlller.createFavorite, (req, res) => {
+router.post('/', jwtController.verify, favoriteController.createFavorite, (req, res) => {
   res.status(200).json('GOOOOOD');
-}); 
-
+});
+//post user/fav/
 // get all fav 
-router.get('/', favoriteController.getFavorite, (req, res) => {
+router.get('/',jwtController.verify, favoriteController.getFavorite, (req, res) => {
   // send back to frontend
 })
 
